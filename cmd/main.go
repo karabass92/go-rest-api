39a182a1f74ac4841b4b744b0dfd5c2b611d88f9
karabass92 <shatveryank@gmail.com>
2025-05-06 20:20:32 +0500

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	todo "github.com/karabass92/go-rest-api"
 	"github.com/karabass92/go-rest-api/pkg/handler"
+	"github.com/karabass92/go-rest-api/pkg/repository"
+	"github.com/karabass92/go-rest-api/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
